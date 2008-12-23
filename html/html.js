d9e1ee059fba8b992603f36a06e4f0f0eaab0b81
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2008-12-23 15:06:56 +0000

    Добавлены определения основных тегов

--- html/html.js
+++ html/html.js
@@ -2,18 +2,29 @@ var Html = {};
 
 (function(){
 	function extend(o,s){for(var k in s){o[k] = s[k];}}
+	
 	function each(coll, F){
 		for(var i=0; i<coll.length;i++){
 			F(coll[i], i);
 		}
 	}
 	
-	function genericTag(name){
-		return new Function("content", "return Html.tag(\""+name+"\", arguments);");
+	function defineTags(tags){
+		each(tags, function(t){
+			Html[t] = new Function("content", "return Html.tag(\""+t+"\", arguments);");
+		});
+	}
+	
+	function defineSelfClosingTags(tags){
+		each(tags, function(t){
+			Html[t] = new Function("content", "return Html.tag(\""+t+"\", arguments, true);");
+		});
 	}
 	
 	extend(Html, {
-		tag: function(name, content){
+		xhtmlMode: true,
+		
+		tag: function(name, content, selfClosing){
 			var h = [];
 			var a = [];
 			each(content, function(el){
@@ -25,10 +36,14 @@ var Html = {};
 				else
 					h.push(el);
 			});
-			return "<"+name+a.join("")+">"+h.join("")+"</"+name+">";
-		},
-		
-		div: genericTag("div"),
-		p: genericTag("p")
+			
+			if(selfClosing && h.length==0)
+				return "<"+name+a.join("")+(Html.xhtmlMode? "/>":">");
+			else
+				return "<"+name+a.join("")+">"+h.join("")+"</"+name+">";
+		}
 	});
+	
+	defineTags(["div", "p", "span", "ul", "ol", "li", "table", "tbody", "thead", "tr", "th", "td", "input", "textarea", "pre"]);
+	defineSelfClosingTags(["img", "hr", "br"]);
 })();
