80bad09d9aa1d2c852dba50fa06d4491a2e88cfe
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-01-12 12:28:50 +0000

    Добавлены некоторые стандартные функции принадлежности

--- fuzzy/fuzzy.js
+++ fuzzy/fuzzy.js
@@ -5,7 +5,7 @@ var Fuzzy = {};
 	
 	var _ = Fuzzy;
 	extend(_, {
-		version: "1.1.52",
+		version: "1.2.53",
 		
 		and: function(x, y){
 			return Math.min(_.getFuzzyValue(x),_.getFuzzyValue(y));
@@ -17,6 +17,38 @@ var Fuzzy = {};
 		
 		not: function(x){
 			return 1 - _.getFuzzyValue(x);
+		},
+		
+		functions: {
+			S: function(vMin, vMax){
+				return function(x){
+					return x>vMax? 1
+						:x<vMin? 0
+						:(x - vMin)/(vMax - vMin);
+				};
+			},
+			Z: function(vMin, vMax){
+				return function(x){
+					return x>vMax? 0
+						:x<vMin? 1
+						:(x-vMax)/-(vMax - vMin);
+				};
+			}
+		},
+		
+		defuzzy: function(xMin, xMax, F){
+			var count = 100;
+			var points = 0;
+			var step = (xMax - xMin)/count;
+			var S = 0;
+			for(var p = xMin; p<=xMax; p+=step){
+				var val = F(p);
+				if(val>0){
+					points++;
+					S+=val*p;
+				}
+			}
+			return S/count + xMin;
 		},
 		
 		getFuzzyValue: function(x){
