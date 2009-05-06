46da7b004649d8b2c640e20dc48c42c7368b1c03
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-05-06 10:54:52 +0000

    Research, Optimization: вернули функцию стравнения итераторам

--- optimization/opt.js
+++ optimization/opt.js
@@ -10,89 +10,109 @@ var Opt = {};
 	}
 	
 	extend(Opt, {
-		version: "2.0.0",
+		version: "2.1.0",
 		maxIterations:100,
 		
-		findMin: function(F, eps, iterator){
-			var res = F(iterator.$val());
-			var d = 0;
+		findMin: function(F, iterator){
+			var res = F(iterator.x);
 			for(var i=0; i<Opt.maxIterations; i++){
-				var x1 = iterator.next(d>0);
+				var x1 = iterator.next();
 				var res1 = F(x1);
-				d = res1 - res;
+				iterator.compare(res, res1);
 				//console.log(i,": ",iterator.x, res1, iterator.dy);
-				if(Math.abs(d)<eps) break;
+				if(iterator.$end()) break;
 				res = res1;
 			}
 			
 			//console.log("found in "+i+"steps");
-			return {min:x1, esp:d, steps:i};
+			return {min:iterator.x, esp:iterator.dy, steps:i};
 		}
 	});
 	
-	Opt.scalarIterator = function(x0, dx0){var _=this;
+	Opt.scalarIterator = function(x0, dx0, eps){var _=this;
 		_.x0 = x0;
 		_.dx0 = dx0;
+		_.eps = eps;
 		_.reset();
 	};
 	
 	Opt.scalarIterator.prototype = {
 		x:0,
 		dx:0,
+		dy:0,
+		
+		$end:function(){var _=this;
+			return Math.abs(_.dy)<_.eps;
+		},
 		
 		reset: function(){var _=this;
 			_.x = _.x0;
 			_.dx = _.dx0;
 		},
 		
-		next: function(reverse){var _=this;
-			if(reverse){
-				//console.log("reverse");
+		compare: function(v0, v1){var _=this;
+			_.dy = v1 - v0;
+			return _.dy;
+		},
+		
+		next: function(){var _=this;
+			if(_.dy>0){
+				//console.log("reverse on dy="+_.dy);
 				_.dx = _.dx*-0.5;
 			}
 			_.x+=_.dx;
 			return _.x;
-		},
-		
-		$val: function(){
-			return this.x;
 		}
 	};
 	
-	Opt.vectorIterator = function(v0, dv0){var _=this;
-		_.v0 = v0;
-		_.dv0 = dv0;
+	Opt.vectorIterator = function(x0, dx0, eps){var _=this;
+		_.x0 = x0;
+		_.dx0 = dx0;
+		_.eps = eps;
+		
 		_.iterators = [];
-		each(v0, function(x, i){
-			_.iterators[i] = new Opt.scalarIterator(x, dv0[i]);
+		each(x0, function(x, i){
+			_.iterators[i] = new Opt.scalarIterator(x, dx0[i]);
 		});
 		_.reset();
 	}
 	
 	Opt.vectorIterator.prototype = {
-		v:[],
-		dv:[],
+		x:0,
+		dx:0,
+		dy:0,
+		
+		$end:function(){var _=this;
+			var end = true;
+			each(this.iterators, function(itr){
+				end = end && itr.$end();
+			});
+			return end;
+		},
+		
 		iterators:[],
 		iteratorIdx:0,
 		
 		reset: function(){var _=this;
-			_.v = _.v0;
-			_.dv = _.dv0;
+			_.x = _.x0;
+			_.dx = _.dx0;
 			each(_.iterators, function(itr){
 				itr.reset();
 			});
 		},
 		
-		next: function(reverse){var _=this;
+		compare: function(v0, v1){var _=this;
+			return _.iterators[_.iteratorIdx].compare(v0, v1);
+		},
+		
+		next: function(){var _=this;
 			// Последовательный обход по всем координатам.
-			// Если включается реверс - возвращаемся по той же координате
-			if(!reverse){
-				_.iteratorIdx++;
-				if(_.iteratorIdx>_.iterators.length-1) _.iteratorIdx = 0;
-			}
-			console.log("iterator "+_.iteratorIdx+(reverse?" reverse":""));
-			_.v[_.iteratorIdx] = _.iterators[_.iteratorIdx].next(reverse);
-			return _.v;
+			_.iteratorIdx++;
+			if(_.iteratorIdx>_.iterators.length-1) _.iteratorIdx = 0;
+			
+			//console.log("iterator "+_.iteratorIdx+(reverse?" reverse":""));
+			_.x[_.iteratorIdx] = _.iterators[_.iteratorIdx].next();
+			return _.x;
 		},
 		
 		$val: function(){
