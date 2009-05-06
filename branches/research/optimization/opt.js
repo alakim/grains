c25aa4004309902ece2dae39ef2d078e0fb3c59b
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-05-06 07:53:35 +0000

    Research, Optimization: абстрагирован итератор по значениям целевой функции

--- branches/research/optimization/opt.js
+++ branches/research/optimization/opt.js
@@ -3,33 +3,57 @@ var Opt = {};
 (function(){
 	function extend(o, s){for(var k in s)o[k]=s[k];}
 	
-	var _ = Opt;
-	
-	extend(_, {
-		version: "0.0.0",
+	extend(Opt, {
+		version: "1.0.0",
+		maxIterations:100,
 		
-		findMin: function(F, x0, dx0, eps){
-			var maxCount = 100;
-			var res = F(x0);
-			var x = x0;
-			var dx = dx0;
-			var d = 0;
-			for(var i=0; i<maxCount; i++){
-				var x1 = x+dx;
+		findMin: function(F, iterator){
+			var res = F(iterator.x);
+			for(var i=0; i<Opt.maxIterations; i++){
+				var x1 = iterator.next();
 				var res1 = F(x1);
-				d = Math.abs(res - res1);
-				//console.log(i,": ",x, res1, d);
-				if(d<eps) break;
-				if(res1<res){
-					res = res1;
-					x = x1;
-				}
-				else
-					dx = dx*-0.5;
+				iterator.compare(res, res1);
+				//console.log(i,": ",iterator.x, res1, iterator.dy);
+				if(iterator.end) break;
+				res = res1;
 			}
 			
 			//console.log("found in "+i+"steps");
-			return {min:x, esp:d, steps:i};
+			return {min:iterator.x, esp:iterator.dy, steps:i};
 		}
 	});
+	
+	Opt.scalarIterator = function(x0, dx0, eps){var _=this;
+		_.x0 = x0;
+		_.dx0 = dx0;
+		_.eps = eps;
+		_.reset();
+	};
+	
+	Opt.scalarIterator.prototype = {
+		x:0,
+		dx:0,
+		dy:0,
+		end:false,
+		
+		reset: function(){var _=this;
+			_.x = _.x0;
+			_.dx = _.dx0;
+		},
+		
+		compare: function(v0, v1){var _=this;
+			_.dy = v1 - v0;
+			_.end = Math.abs(_.dy)<_.eps;
+			return _.dy;
+		},
+		
+		next: function(){var _=this;
+			if(_.dy>0){
+				//console.log("reverse on dy="+_.dy);
+				_.dx = _.dx*-0.5;
+			}
+			_.x+=_.dx;
+			return _.x;
+		}
+	};
 })();
