57b805058ba0455667d235118c108b55e75ad78c
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-05-06 08:31:15 +0000

    Research, Optimization: пробная, не отлаженная версия векторного итератора

--- optimization/opt.js
+++ optimization/opt.js
@@ -3,8 +3,14 @@ var Opt = {};
 (function(){
 	function extend(o, s){for(var k in s)o[k]=s[k];}
 	
+	each(coll, F){
+		if(!coll) return;
+		for(var i=0; i<coll.length; i++)
+			F(coll[i], i);
+	}
+	
 	extend(Opt, {
-		version: "1.1.0",
+		version: "2.0.0",
 		maxIterations:100,
 		
 		findMin: function(F, eps, iterator){
@@ -20,11 +26,11 @@ var Opt = {};
 			}
 			
 			//console.log("found in "+i+"steps");
-			return {min:iterator.x, esp:d, steps:i};
+			return {min:x1, esp:d, steps:i};
 		}
 	});
 	
-	Opt.scalarIterator = function(x0, dx0, eps){var _=this;
+	Opt.scalarIterator = function(x0, dx0){var _=this;
 		_.x0 = x0;
 		_.dx0 = dx0;
 		_.reset();
@@ -41,11 +47,47 @@ var Opt = {};
 		
 		next: function(reverse){var _=this;
 			if(reverse){
-				//console.log("reverse on dy="+_.dy);
+				//console.log("reverse");
 				_.dx = _.dx*-0.5;
 			}
 			_.x+=_.dx;
 			return _.x;
 		}
 	};
+	
+	Opt.vectorIterator = function(v0, dv0){var _=this;
+		_.v0 = v0;
+		_.dv0 = dv0;
+		_.iterators = [];
+		each(v0, function(x, i){
+			_.iterators[i] = new Opt.scalarIterator(x, dv0[i]);
+		});
+		_.reset();
+	}
+	
+	Opt.vectorIterator.prototype = {
+		v:[],
+		dv:[],
+		iterators:[],
+		iteratorIdx:0,
+		
+		reset: function(){var _=this;
+			_.v = _.v0;
+			_.dv = _.dv0;
+			each(_.iterators, function(itr){
+				itr.reset();
+			});
+		},
+		
+		next: function(reverse){var _=this;
+			// Последовательный обход по всем координатам.
+			// Если включается реверс - возвращаемся по той же координате
+			if(!reverse){
+				_.iteratorIdx++;
+				if(_.iteratorIdx>_.iterators.length-1) _.iteratorIdx = 0;
+			}
+			_.v[_.iteratorIdx] = _.iterators[_.iteratorIdx].next(reverse);
+			return _.v;
+		}
+	}
 })();
