784719ece0746baa9f4873a61b20f52231fadaf9
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-05-15 06:31:09 +0000

    JSQL. Предварительная реализация

--- branches/research/jsql/jsql.js
+++ branches/research/jsql/jsql.js
@@ -11,5 +11,35 @@ var JSQL = {};
 	
 	extend(JSQL, {
 		version: "0.0.0",
+		
+		DataBase: function(data){var _=this;
+			_.tablesCount = 0;
+			_.tables = {};
+			each(data, function(itm){
+				if(itm.constructor==JSQL.Table){
+					_.tablesCount++;
+					if(!(itm.name && itm.name.length))
+						itm.name = "table"+_.tablesCount;
+					_.tables[itm.name] = itm;
+				}
+			});
+		},
+		
+		Table: function(name, rows){
+			this.name = name;
+			this.rows = rows;
+		}
 	});
+	
+	JSQL.DataBase.prototype = {
+		select: function(column, statements){var _=this;
+			var table = _.tables[statements.from]
+			var res = [];
+			each(table.rows, function(row){
+				if(row[column])
+					res.push(row[column]);
+			});
+			return res;
+		}
+	};
 })();
