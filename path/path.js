1718f3beb8f3531912f7a9224fc449dab26f975f
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-05-26 09:47:33 +0000

    Добавлен модуль Path

---
+++ path/path.js
@@ -0,0 +1,95 @@
+function Path(path){var _=this;
+	_.file = null;
+	if(path.constructor==Array)
+		_.dirs = path;
+	else if(typeof(path)=="object" && path.dirs){
+		_.file = path.file;
+		_.dirs = [];
+		for(var i=0; i<path.dirs.length; i++){
+			_.dirs.push(path.dirs[i]);
+		}
+	}
+	else if(typeof(path)=="string"){
+		_.dirs = [];
+		var steps = path.split(/[\\\/]/);
+		for(var i=0; i<steps.length; i++){
+			var s = steps[i];
+			if(s.match(/^[^\.\\\/]+(\.\w+)+$/i))
+				_.file = s;
+			else
+				_.dirs.push(s);
+		}
+	}
+	else
+		_.dirs = [];
+}
+
+(function(){
+	function extend(o, s){for(var k in s)o[k]=s[k];}
+	
+	var __ = Path;
+	
+	extend(__, {
+		version: "1.0.0",
+		maxIterationCount:500
+	});
+	
+	Path.prototype = {
+		toDos: function(){var _=this;
+			return _.dirs.join("\\")+(_.file?("\\"+_.file):"");
+		},
+		
+		toWeb: function(){var _=this;
+			return _.dirs.join("/")+(_.file?("/"+_.file):"");
+		},
+		
+		relativeTo: function(basePath){var _=this;
+			if(typeof(basePath)=="string")
+				basePath = new Path(basePath);
+			
+			var backward = [];
+			var forward = [];
+				
+			for(var i=0; i<__.maxIterationCount; i++){
+				if(i>=_.dirs.length || i>=basePath.dirs.length)
+					break;
+				if(_.dirs[i]==basePath.dirs[i])
+					continue;
+				if(basePath.dirs.length>i)
+					backward.push("../");
+				if(_.dirs.length>i)
+					forward.push(_.dirs[i]);
+			}
+			if(i>=__.maxIterationCount){
+				throw "Too many iterations";
+			}
+			
+			return new Path(backward.join("")+forward.join("/")+(_.file?("/"+_.file):""));
+		},
+		
+		absoluteFrom: function(basePath){var _=this;
+			if(typeof(basePath)=="string")
+				basePath = new Path(basePath);
+				
+			var baseIdx = basePath.length;
+			var forward = [];
+			for(var i=0; i<_.dirs.length; i++){
+				var d = _.dirs[i];
+				console.log(d);
+				if(d=="..")
+					baseIdx--;
+				else
+					forward.push(d);
+			}
+			
+			var res = [];
+			for(var i=0; i<baseIdx; i++){
+				res.push(basePath.dirs[i]);
+			}
+			
+			return new Path(res.join("/")+"/"+forward.join("/")+(_.file?("/"+_.file):""));
+		}
+	};
+})()
+
+
