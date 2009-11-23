0f4eff369a1a63323fd355eb9892460135d58c91
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-11-23 16:03:18 +0000

    Начата доработка модуля jsDoc

--- tools/jsDoc.js
+++ tools/jsDoc.js
@@ -1,3 +1,5 @@
+if(typeof(Html)=="undefined") alert("Configuration error!\n html.js module required.");
+
 function Documentation(objName, description, defs){
 	this.description = description;
 	this.idx = Documentation.instances.length;
@@ -7,94 +9,108 @@ function Documentation(objName, description, defs){
 	
 }
 
-Documentation.prototype = {
-	buildView: function(div){
-		var html = [];
-		html.push("<div class=\"globalObject\">")
-		html.push("<h2><a name=\"inst"+this.idx+"\">"+this.objName+"</a></h2>");
-		if(this.description)
-			html.push("<p>"+this.description+"</p>");
-		var obj;
-		obj = window[this.objName];
-		if(obj){
-			html.push(this._buildHtml(obj, this.defs));
-		}
-		html.push("</div>");
-		div.innerHTML = html.join("");
-	},
+Documentation.version = "1.0.0";
+Documentation.sources = [];
+
+(function(){
+	function $(id){return document.getElementById(id);}
 	
-	_buildHtml: function(obj, defs){
-		if(!obj)
-			return "<li style=\"color:#ff0000;\"><span style=\"font-weight:bold;\">"+name+"</span> item not implemented</li>";
-		var html = [];
-		html.push("<ol>");
-		for(var k in obj){
-			var doc = this.getDoc(k, defs);
-			var docString = doc!=null
-				?(doc.type+", "+doc.dsc)
-				:"<span style=\"color:#cccccc;\">not documented</span>";
-				
-			html.push("<li><span style=\"font-weight:bold;\">"+k+":</span> "+docString);
-			if(doc!=null && doc.children){
-				var chObj = obj[k];
-				if(!chObj)
-					html.push("Object '"+k+"' not found");
-				else{
-					html.push(this._buildHtml(chObj, doc.children));
-				}
+	function extend(o,s){for(var k in s)o[k]=s[k];}
+	
+	var __=Documentation;
+	
+	extend(__, {
+		instances:[],
+		
+		show:function(){
+			var toc = document.createElement("DIV");
+			document.body.appendChild(toc);
+			var html = [];
+			for(var i=0;i<Documentation.instances.length;i++){
+				var inst = Documentation.instances[i];
+				html.push("<a href=\"#inst"+inst.idx+"\">"+inst.objName+"</a><br>");
 			}
-			html.push("</li>");
+			toc.innerHTML = html.join("");
+			toc.style.marginBottom = 15;
+			
+			for(var i=0;i<Documentation.instances.length;i++){
+				var outdiv = document.createElement("DIV");
+				document.body.appendChild(outdiv);
+				var inst = Documentation.instances[i];
+				inst.buildView(outdiv);
+			}
 		}
-		for(var i=0;i<defs.length;i++){
-			var def = defs[i];
-			if(def.type=="instance"){
-				html.push("<li><span style=\"font-weight:bold;\">object instance:</span>");
-				html.push("<ol>");
-				for(var i=0;i<def.children.length;i++){
-					var ch = def.children[i];
-					html.push("<li><span style=\"font-weight:bold;\">"+ch.name+":</span> "+ch.type+", "+ch.dsc+"</li>");
+	});
+	
+	__.prototype = {
+		buildView: function(outdiv){with(Html){var _=this;
+			var obj = window[_.objName];
+			outdiv.innerHTML = div({"class":"globalObject"},
+				div("Sources: ",
+					apply(Documentation.sources, function(src){
+						return div(span({style:"font-weight:bold;"}, src), ": ", $(src).src);
+					})
+				),
+				h2(a({name:"inst"+_.idx}, _.objName)),
+				_.description?p(_.description):null,
+				obj?_._buildHtml(obj, _.defs):null,
+				
+				p({style:"text-align:center; margin-top:80px; font-size:10px; font-family:Tahoma, Arial, Sans-serif; color:#cccccc; border-top:1px solid #cccccc;"}, "Powered by jsDoc v.", Documentation.version)
+			)
+		}},
+		
+		_buildHtml: function(obj, defs){
+			if(!obj)
+				return "<li style=\"color:#ff0000;\"><span style=\"font-weight:bold;\">"+name+"</span> item not implemented</li>";
+			var html = [];
+			html.push("<ol>");
+			for(var k in obj){
+				var doc = this.getDoc(k, defs);
+				var docString = doc!=null
+					?(doc.type+", "+doc.dsc)
+					:"<span style=\"color:#cccccc;\">not documented</span>";
+					
+				html.push("<li><span style=\"font-weight:bold;\">"+k+":</span> "+docString);
+				if(doc!=null && doc.children){
+					var chObj = obj[k];
+					if(!chObj)
+						html.push("Object '"+k+"' not found");
+					else{
+						html.push(this._buildHtml(chObj, doc.children));
+					}
 				}
-				html.push("</ol>");
 				html.push("</li>");
 			}
-			else if(!def.displayed){
-				html.push("<li style=\" color:#aaaaaa;\"><span style=\"font-weight:bold;\">"+def.name+":</span> "+def.type+", "+def.dsc+" (not implemented)</li>");
+			for(var i=0;i<defs.length;i++){
+				var def = defs[i];
+				if(def.type=="instance"){
+					html.push("<li><span style=\"font-weight:bold;\">object instance:</span>");
+					html.push("<ol>");
+					for(var i=0;i<def.children.length;i++){
+						var ch = def.children[i];
+						html.push("<li><span style=\"font-weight:bold;\">"+ch.name+":</span> "+ch.type+", "+ch.dsc+"</li>");
+					}
+					html.push("</ol>");
+					html.push("</li>");
+				}
+				else if(!def.displayed){
+					html.push("<li style=\" color:#aaaaaa;\"><span style=\"font-weight:bold;\">"+def.name+":</span> "+def.type+", "+def.dsc+" (not implemented)</li>");
+				}
 			}
-		}
-		html.push("</ol>");
-		return html.join("");
-	},
-	
-	getDoc: function(itemName, defs){
-		for(var i=0;i<defs.length;i++){
-			var def = defs[i];
-			if(def.name==itemName){
-				def.displayed = true;
-				return def;
+			html.push("</ol>");
+			return html.join("");
+		},
+		
+		getDoc: function(itemName, defs){
+			for(var i=0;i<defs.length;i++){
+				var def = defs[i];
+				if(def.name==itemName){
+					def.displayed = true;
+					return def;
+				}
 			}
+			return null;
 		}
-		return null;
 	}
-}
 
-Documentation.instances = [];
-
-Documentation.show = function(){
-	
-	var toc = document.createElement("DIV");
-	document.body.appendChild(toc);
-	var html = [];
-	for(var i=0;i<Documentation.instances.length;i++){
-		var inst = Documentation.instances[i];
-		html.push("<a href=\"#inst"+inst.idx+"\">"+inst.objName+"</a><br>");
-	}
-	toc.innerHTML = html.join("");
-	toc.style.marginBottom = 15;
-	
-	for(var i=0;i<Documentation.instances.length;i++){
-		var div = document.createElement("DIV");
-		document.body.appendChild(div);
-		var inst = Documentation.instances[i];
-		inst.buildView(div);
-	}
-}
+})();
---
+++ tools/jsDocTests/testModule.js
@@ -0,0 +1,12 @@
+var testObject = {
+	id: "abc",
+	toString: function(){
+		return "testObject:"+this.id;
+	},
+	userName: "ivanov",
+	userData: {
+		fio: "Иванов И.И.",
+		phone: "444-44-44",
+		address: "ул. Новаторов, 3-11"
+	}
+}
