53fcff5d30f27c251f9c9f77e2a903117530b621
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2009-12-11 14:12:35 +0000

    Slider Module: рефакторинг

--- trunk/slider/slider.js
+++ trunk/slider/slider.js
@@ -13,149 +13,57 @@ Slider.version = "1.0.0";
 			element.attachEvent("on"+event, handler);
 	}
 	
-	function Position(x,y){
-		this.x = x?x:0;
-		this.y = y?y:0;
-	}
-	
-	Position.get = function(el){
-		if(!el.style) return new Position();
-		return new Position(
-			parseInt(el.style.left),
-			parseInt(el.style.top)
-		);
-	};
-	
-	Position.prototype = {
-		set: function(el){
-			el.style.left = this.x+"px";
-			el.style.top = this.y+"px";
-		},
-		
-		add:function(offset){
-			return new Position(
-				this.x+=offset.x,
-				this.y+=offset.y
-			);
-		},
-		
-		sub:function(offset){
-			return new Position(
-				this.x-=offset.x,
-				this.y-=offset.y
-			);
-		},
-		
-		offset: function(x, y){
-			this.x+=(x||0);
-			this.y+=(y||0);
-		}
-	};
-	
 	var __=Slider;
 	
+	var distance;
+	var initPos;
+	var targetObject;
+	var initMousePos;
 	
+	function capture(e){
+		e = window.event || e;
+		var t=this;
+		targetObject=this;
+		initMousePos = {x:e.clientX, y:e.clientY};
+		initPos = {x:parseInt(t.style.left), y:parseInt(t.style.top)};
+		document.onmousemove = moving;
+		document.onmouseup = stop;
+		return false;
+	}
+
+	function moving(e){
+		e = window.event || e;
+		
+		distance = {
+			x: e.clientX - initMousePos.x,
+			y: e.clientY - initMousePos.y
+		};
+		
+		var settings = targetObject.sliderSettings;
+		if(!settings.lockX)
+			targetObject.style.left = (distance.x + initPos.x) + "px";
+		if(!settings.lockY)
+			targetObject.style.top  = (distance.y + initPos.y) + "px";
+		
+		return false;
+	}
+
+					
+	function stop(){
+		targetObject = null;
+		document.onmousemove = null;
+		document.onmouseup = null;
+	}
+
 	extend(__, {
-		init: function(targetObject){
-			if(typeof(targetObject)=="string") targetObject = $(targetObject);
+		init: function(target, settings){
+			if(typeof(target)=="string") target = $(target);
+			settings = settings||{};
+			target.sliderSettings = settings;
 			
-			function config(el, resizeMode){
-				el.onmousedown = __.capture;
-				el.style.cursor = "move";
-			}
-			
-			config(targetObject);
-		},
-		
-		moving:function(e){
-			e = window.event || e;
-			var targetObject = __.targetObject;
-			
-			__.distance = {
-				x: e.clientX - __.initMousePos.x,
-				y: e.clientY - __.initMousePos.y
-			};
-			
-			targetObject.style.left = (__.distance.x + __.initPos.x) + "px";
-			targetObject.style.top  = (__.distance.y + __.initPos.y) + "px";
-			
-			return false;
-		},
-		
-		stop:function(){
-			__.targetObject = null;
-			document.onmousemove = null;
-			document.onmouseup = null;
-		},
-		
-		capture:function(e){
-			e = window.event || e;
-			var t=this;
-			__.targetObject=this;
-			__.initMousePos = {x:e.clientX, y:e.clientY};
-			__.initPos = Position.get(t);
-			document.onmousemove=__.moving;
-			document.onmouseup=__.stop;
-			return false;
-		}
+			target.onmousedown = capture;
+			target.style.cursor = settings.lockY?"ew-resize":settings.lockX?"ns-resize":"move";
+		}		
 	});
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/*	var instances = [];
-	
-	extend(__, {
-		captured:null,
-		capturedPos:null,
-		mousedown: function(e, el){
-			e = window.event||e;
-			__.capturedPos = new Position(e.clientX, e.clientY);
-			var elPos = Position.get(el);
-			__.capturedPos = elPos.sub(__.capturedPos);
-			__.captured = el.slider;
-		},
-		mouseup: function(){__.captured = null;},
-		mousemove: function(e){
-			if(__.captured) __.captured.mousemove(window.event||e);
-		}
-	});
-	
-	__.prototype = {
-		elID:null,
-		id:null,
-		$el: function(){return $(this.elID);},
-		
-		init:function(){var _=this;
-			_.id = instances.length;
-			instances.push(_);
-			_.offset = new Position();
-			_.scroll = new Position();
-			
-			_.$el().onmousedown = new Function("e", "Slider.mousedown(e, this);");
-			_.$el().slider = _;
-		},
-		
-		mousemove: function(e){var _=this;
-			var pos = new Position(e.clientX-15, e.clientY-15);
-			pos = pos.add(__.capturedPos);
-			pos.set(_.$el());
-		}
-	};
-	
-	addEventHandler(document, "mousemove", __.mousemove);
-	addEventHandler(document, "mouseup", __.mouseup);*/
 })();
