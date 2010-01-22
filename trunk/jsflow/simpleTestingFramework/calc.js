00a11cad10e43c28ea35e2661e27b390d9b99bd6
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2010-01-22 08:58:15 +0000

    JSFlow: в свойства объекта Continuation добавлена ссылка на вызывающий блок. Добавлен пример построения простой оболочки для тестирования асинхронных процессов.

---
+++ trunk/jsflow/simpleTestingFramework/calc.js
@@ -0,0 +1,30 @@
+﻿// Объект тестирования - калькулятор
+
+function Calc(){
+}
+
+(function(){
+	Calc.prototype = {
+		delay: 800,
+		
+		add: function(x, y, onready){var _=this;
+			var res = x+y;
+			window.setTimeout(function(){onready(res);}, _.delay);
+		},
+		
+		sub: function(x, y, onready){var _=this;
+			var res = x-y;
+			window.setTimeout(function(){onready(res);}, _.delay);
+		},
+		
+		mul: function(x, y, onready){var _=this;
+			var res = x*y;
+			window.setTimeout(function(){onready(res);}, _.delay);
+		},
+		
+		div: function(x, y, onready){var _=this;
+			var res = x/y;
+			window.setTimeout(function(){onready(res);}, _.delay);
+		}
+	};
+})();
