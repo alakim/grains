f726982c493f17558cee47326831e51c71312c03
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2010-06-23 06:13:34 +0000

    jColl: рефакторинг конструктора

--- branches/research/collections/jcoll.js
+++ branches/research/collections/jcoll.js
@@ -7,99 +7,112 @@ var jColl = (function(){
 		return F;
 	}
 	
+	function extend(o,s){for(var k in s)o[k] = s[k];}
+	
+	function Mon(coll){
+		extend(this, {
+			map:function(F){
+				F = lambda(F);
+				var res;
+				if(coll instanceof Array){
+					res = [];
+					for(var i=0; i<coll.length; i++) res.push(F(coll[i], i));
+				}
+				else{
+					res = {};
+					for(var k in coll) res[k] = F(coll[k], k);
+				}
+				return _.$C(res);
+			},
+			
+			toList: function(F){
+				F = lambda(F);
+				var res = [];
+				for(var k in coll) res.push(F(coll[k], k));
+				return _.$C(res);
+			},
+			
+			toDict: function(keyF, valF){
+				keyF = lambda(keyF);
+				valF = lambda(valF);
+				var res = {};
+				for(var i=0; i<coll.length; i++){var e = coll[i];
+					res[keyF(e,i)] = valF(e,i);
+				}
+				return _.$C(res);
+			},
+			
+			path: function(p){
+				var steps = p.split("/");
+				var r = coll;
+				for(var i=0; i<steps.length; i++){
+					r = r[steps[i]];
+				}
+				return _.$C(r);
+			},
+			
+			first: function(){
+				return _.$C(coll[0]);
+			},
+			
+			last: function(){
+				return _.$C(coll[coll.length-1]);
+			},
+			
+			filter: function(P){
+				P = lambda(P);
+				var res;
+				if(coll instanceof Array){
+					res = [];
+					for(var i=0; i<coll.length; i++)
+						if(P(coll[i], i))
+							res.push(coll[i])
+				}
+				else{
+					res = {};
+					for(var k in coll)
+						if(P(coll[k], k))
+							res[k] = coll[k];
+				}
+				return _.$C(res);
+			},
+			
+			find: function(P){
+				P = lambda(P);
+				if(coll instanceof Array){
+					for(var i=0; i<coll.length; i++){var el = coll[i];
+						if(P(el, i)) return _.$C(el);
+					}
+				}
+				else{
+					for(var k in coll){var el = coll[k];
+						if(P(el, k)) return _.$C(el);
+					}
+				}
+				return _.$C();
+			},
+			
+			join: function(coll2, F){
+				F = lambda(F);
+				var r = [];
+				for(var k in coll){
+					var e = coll[k];
+					for(var m in coll2){
+					}
+				}
+				return _.$C(r)
+			},
+			
+			item:function(){
+				return coll;
+			}
+		});
+	}
+	
 	var _ = {
 		version:"1.0.0",
 		$C: function(coll){
-			return {
-				map:function(F){
-					F = lambda(F);
-					var res;
-					if(coll instanceof Array){
-						res = [];
-						for(var i=0; i<coll.length; i++) res.push(F(coll[i], i));
-					}
-					else{
-						res = {};
-						for(var k in coll) res[k] = F(coll[k], k);
-					}
-					return _.$C(res);
-				},
-				
-				toList: function(F){
-					F = lambda(F);
-					var res = [];
-					for(var k in coll) res.push(F(coll[k], k));
-					return _.$C(res);
-				},
-				
-				toDict: function(keyF, valF){
-					keyF = lambda(keyF);
-					valF = lambda(valF);
-					var res = {};
-					for(var i=0; i<coll.length; i++){var e = coll[i];
-						res[keyF(e,i)] = valF(e,i);
-					}
-					return _.$C(res);
-				},
-				
-				path: function(p){
-					var steps = p.split("/");
-					var r = coll;
-					for(var i=0; i<steps.length; i++){
-						r = r[steps[i]];
-					}
-					return _.$C(r);
-				},
-				
-				first: function(){
-					return _.$C(coll[0]);
-				},
-				
-				last: function(){
-					return _.$C(coll[coll.length-1]);
-				},
-				
-				filter: function(P){
-					P = lambda(P);
-					var res;
-					if(coll instanceof Array){
-						res = [];
-						for(var i=0; i<coll.length; i++)
-							if(P(coll[i], i))
-								res.push(coll[i])
-					}
-					else{
-						res = {};
-						for(var k in coll)
-							if(P(coll[k], k))
-								res[k] = coll[k];
-					}
-					return _.$C(res);
-				},
-				
-				find: function(P){
-					P = lambda(P);
-					if(coll instanceof Array){
-						for(var i=0; i<coll.length; i++){var el = coll[i];
-							if(P(el, i)) return _.$C(el);
-						}
-					}
-					else{
-						for(var k in coll){var el = coll[k];
-							if(P(el, k)) return _.$C(el);
-						}
-					}
-					return _.$C();
-				},
-				
-				join: function(coll2, F){
-					
-				},
-				
-				item:function(){
-					return coll;
-				}
-			};
+			return coll instanceof Mon?coll:new Mon(coll);
 		}
 	};
 	
