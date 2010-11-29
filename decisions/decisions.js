f139c003484a21ada49f4737be75a4ddb0a5f04d
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2010-11-29 11:18:35 +0000

    Полностью переработан проект Decisions. Добавлена возможность указания критериев выбора решения.

--- decisions/decisions.js
+++ decisions/decisions.js
@@ -1,137 +1,349 @@
-if(typeof(Html)=="undefined") alert("html.js module required!");
-
-function abstractNode(argList, inst, type){
-	argList = argList[0];
-	inst.type = type;
-	inst.content = argList[0];
-	inst.children = [];
-	for(var i=1; i<argList.length; i++){
-		var el = argList[i];
-		inst.children.push(el);
-		el.parent = inst;
-	}
-}
-
-var stdBorderStyle = "border:1px solid #aaaaaa; border-left-width:3px; padding:3px; margin:3px; margin-left:10px;";
-
-function Icon(text, size, bgColor, color){
-	bgColor = bgColor?bgColor:"#ffffff";
-	color = color?color:"#000000";
-	size = size?size:25;
-	return Html.span({style:"font-size:"+size+"px; padding:3px; margin:3px; background-color:"+bgColor+"; color:"+color+";"}, text);
-}
-
-function Decision(){
-	function c(){abstractNode(arguments, this, "decision");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return div(
-				h1(this.content),
-				apply(this.children, function(chld){
-					return chld.render();
-				})
-			);
-		}}
-	};
-	
-	Decision.instance = new c(arguments);
-}
-
-function Problem(){
-	function c(){abstractNode(arguments, this, "problem");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return div({style:stdBorderStyle},
-				p(
-					Icon("?", 32, "#ffaa88", "#ff0000"),
-					this.content
-				),
-				apply(this.children, function(chld){
-					return chld.render();
-				})
-			);
-		}}
-	};
-	
-	return new c(arguments);
-}
-
-function Solution(){
-	function c(){abstractNode(arguments, this, "solution");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return div({style:stdBorderStyle},
-				p(
-					Icon("!", 32, "#aa88ff", "#00ff00"),
-					this.content
-				),
-				apply(this.children, function(chld){
-					return chld.render();
-				})
-			);
-		}}
-	};
-	
-	return new c(arguments);
-}
-
-function Description(){
-	function c(){abstractNode(arguments, this, "description");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return p({style:"font-size:12px; font-family:Times new roman, serif; font-style:italic; margin-left:20px;"}, this.content);
-		}}
-	};
-	
-	return new c(arguments);
-}
-
-function Advantage(){
-	function c(){abstractNode(arguments, this, "advantage");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return p({style:"margin-left:10px;"},
-				Icon("+", 15, "#00ff00", "#008800"),
-				this.content
-			);
-		}}
-	};
-	
-	return new c(arguments);
-}
-
-function Disadvantage(){
-	function c(){abstractNode(arguments, this, "disadvantage");}
-	
-	c.prototype = {
-		render: function(){with(Html){
-			return p({style:"margin-left:10px;"},
-				Icon("-", 15, "#ff0000", "#880000"),
-				this.content
-			);
-		}}
-	};
-	
-	return new c(arguments);
-}
-
-(function(){
-	
-	function addEventHandler(element, event, handler){
-		if(element.addEventListener)
-			element.addEventListener(event, handler, true);
-		else
-			element.attachEvent("on"+event, handler);
+var Decisions = (function(){
+	function each(coll, F){
+		if(coll instanceof Array) for(var i=0; i<coll.length; i++)F(coll[i], i);
+		else for(var k in coll)F(coll[k], k);
 	}
 	
-	addEventHandler(window, "load", function(){
-		document.body.innerHTML = Decision.instance.render();
-	});
-}());
+	var items = {};
+	var roots = [];
+	function register(itm){
+		itm.id = itm.id || Uid.getNew();
+		items[itm.id] = itm;
+		if(itm.type=="Reasoning") roots.push(itm);
+	}
+	
+	function buildChildren(item, args, argIdx){
+		item.children = [];
+		for(var i=argIdx; i<args.length; i++){var v = args[i];
+			if(v.type=="ID") item.id = v.name;
+			else if(v.type=="Rating") item.rating = v.value;
+			else if(v.type=="SolutionRating"){
+				if(item.type!="Criterion") throw "SolutionRating item applicable only to Criterion item.";
+				if(!item.ratings) item.ratings = {};
+				item.ratings[v.solutionID] = v;
+			}
+			else if(v.type=="Criterion"){
+				if(!item.criteria) item.criteria = [];
+				item.criteria.push(v);
+				
+				if(typeof(v)=="object")v.parent = item;
+				item.children.push(v)
+			}
+			else{
+				if(typeof(v)=="object")
+					v.parent = item;
+				item.children.push(v);
+			}
+		}
+	}
+	
+	var Uid = (function(){
+		var counter = 0;
+		return {
+			getNew: function(){
+				return "uid"+(counter++);
+			},
+			isUid: function(id){
+				return typeof(id)=="string" && id.match(/^uid\d+$/)!=null;
+			}
+		};
+	})();
+	
+	function Reasoning(name){
+		var _ = {
+			type:"Reasoning",
+			name:name,
+			html: function(){with(Html){
+				return div(
+					h1("Reasoning ", this.name),
+					apply(this.children, function(c){
+						return div({style:"margin-left:20px;"},
+							c.html()
+						);
+					})
+				);
+			}}
+		};
+		
+		buildChildren(_, arguments, 1);
+		register(_);
+		return _;
+	}
+	
+	function Problem(name){
+		var itm ={
+			type:"Problem",
+			name:name,
+			html: function(){with(Html){var _=this;
+				function criteriaNorm(){
+					if(!_.criteria) return;
+					var norm = 0;
+					each(_.criteria, function(cr){norm+=cr.rating;});
+					if(norm!=100) return Html.span({style:"backgroundColor:yellow; color:red;"},
+						"Ошибка нормировки. Сумма рейтингов критериев: ", norm, ", норма: 100."
+					);
+				}
+				
+				return tagCollection(
+					h1(
+						{"class":"problem", id:"hdr"+_.id,
+							onclick:callFunction("UI.toggle", _.id),
+							title:"Развернуть"
+						},
+						a({name:_.id},
+							"Задача: ", _.name,
+							Uid.isUid(_.id)?null:span({style:"color:#aaaaee;"}, " [", _.id, "]")
+						)
+					),
+					criteriaNorm(),
+					div({"class":"section", id:"div"+_.id, style:"display:none;"},
+						apply(_.children, function(c){with(Html){
+							return typeof(c.html)=="function"?c.html():c;
+						}}),
+						_.criteria?div({
+							style:"color:#000088; text-decoration:underline; cursor:hand;cursor:pointer",
+							onclick:callFunction("UI.toggleCriteria", _.id)
+						}, "toggle criteria view"):null
+					)
+				);
+			}}
+		};
+		buildChildren(itm, arguments, 1);
+		register(itm);
+		return itm;
+	}
+	
+	function Solution(name){
+		var _={
+			type:"Solution",
+			name:name,
+			html: function(){with(Html){
+				function criteriaTable(){with(Html){
+					return _.parent.criteria?div({style:"margin-top:10px;"},
+						span({style:"font-weight:bold;"}, "Оценки:&#160;"),
+						apply(_.parent.criteria, function(cr){
+							return Html.a(
+								cr.name, ":",
+								cr.ratings[_.id].value,
+								"&#160;"
+							);
+						})
+					):null;
+				}}
 
+				return tagCollection(
+					h2({"class":"solution"},
+						a({"name":_.id}, "Решение: ", _.name,
+							_.date?span(" - ",_.date):null,
+							Uid.isUid(_.id)?null:span({style:"color:#aaaaee;"}, " [", _.id, "]")
+						)
+					),
+					div({"class":"section"},
+						apply(_.children, function(c){
+							return typeof(c.html)=="function"?c.html():c;
+						}),
+						criteriaTable()
+					)
+				);
+			}}
+		};
+		buildChildren(_, arguments, 1);
+		register(_);
+		return _;
+	}
+	
+	function Advantage(){var _=this;
+		var _={
+			type:"Advantage",
+			html: function(){with(Html){var _=this;
 
+	
+				return div(
+					div({style:"color:#008800"}, "Advantage "),
+					div({style:"margin-left:20px;"},
+						apply(_.children, function(c){
+							return div(typeof(c.html)=="function"?c.html():c);
+						})
+					)
+				);
+			}}
+		};
+		buildChildren(_, arguments, 0);
+		register(_);
+		return _;
+	}
+	
+	function Disadvantage(){var _=this;
+		var _={
+			type:"Disadvantage",
+			html: function(){with(Html){
+				return div(
+					div({style:"color:#880000"}, "Disadvantage "),
+					div({style:"margin-left:20px;"},
+						apply(this.children, function(c){
+							return div(typeof(c.html)=="function"?c.html():c);
+						})
+					)
+				);
+			}}
+		};
+		buildChildren(_, arguments, 0);
+		register(_);
+		return _;
+	}
+	
+	function ID(name){var _=this;
+		return {type:"ID", name:name};
+	}
+	
+	function Rating(val){var _=this;
+		return {type:"Rating", value:val};
+	}
+	
+	function SolutionRating(solutionID, val, comment){var _=this;
+		return {
+			type:"SolutionRating",
+			solutionID:solutionID,
+			comment:comment,
+			value:val,
+			html: function(){with(Html){
+				return div(
+					"Solution Rating ", val,
+					span(comment)
+				);
+			}}
+		};
+	}
+	
+	function Description(){
+		var _={
+			html: function(){with(Html){
+				return div({style:"font-style:italic;"},
+					apply(this.children, function(c){
+						return typeof(c.html)=="function"?c.html():c;
+					})
+				);
+			}}
+		};
+		buildChildren(_, arguments, 0);
+		return _;
+	}
+	
+	function Criterion(name){
+		var _={
+			type:"Criterion",
+			name: name,
+			problemID:function(){var _=this;
+				if(_.parent.type!="Problem") throw "Criterion parent must be Problem";
+				return _.parent.id;
+			},
+			html: function(){with(Html){var _=this;
+				var solutionsNorm = 0;
+				each(_.ratings, function(r){solutionsNorm+=r.value;});
+				return div({"class":"transparent", style:"display:none;", problemId:_.problemID()},
+					h2(
+						a({name:_.id}, "Критерий:&#160;", _.name,
+							span({
+								style:"cursor:default;", 
+								titile:"Рейтинг (важность) критерия"
+							}, "(",_.rating,")")
+						)
+					),
+					solutionsNorm!=100?div({style:"backgroundColor:yellow; color:red;"},
+						"Ошибка нормировки. Сумма рейтингов решений: ", solutionsNorm, " (норма: 100)."
+					):null,
+					div({"class":"section"},
+						apply(_.ratings, function(sr, solID){
+							return div(
+								sr.value, "&#160;", items[solID].name, ": ",
+								span({style:"font-style:italic;"}, sr.comment)
+							);
+						})
+					)
+				)
+			}}
+		};
+		buildChildren(_, arguments, 1);
+		return _;
+	}
+		
+	function Conclusion(){
+		var _={
+			html: function(){with(Html){
+				return tagCollection(
+					h2("Заключение"),
+					div({"class":"section"},
+						apply(this.children, function(c){
+							return typeof(c.html)=="function"?c.html():c;
+						})
+					)
+				);
+			}}
+		};
+		buildChildren(_, arguments, 0);
+		return _;
+	}
+	
+	function p(){
+		var _={
+			html: function(){with(Html){
+				return div(
+					apply(this.children, function(c){
+						return typeof(c.html)=="function"?c.html():c;
+					})
+				);
+			}}
+		};
+		buildChildren(_, arguments, 0);
+		return _;
+	}
+	
+	var __ = {
+		version:"2.0.0",
+		display: function(pnlID){
+			var html = [];
+			each(roots, function(r){html.push(r.html());});
+			document.body.innerHTML = html.join(" ");
+		},
+		Reasoning: Reasoning,
+		Problem: Problem,
+		Description: Description,
+		Criterion: Criterion,
+		Rating: Rating,
+		SolutionRating: SolutionRating,
+		Solution:Solution,
+		ID:ID,
+		Conclusion: Conclusion,
+		p:p,
+		Advantage: Advantage,
+		Disadvantage: Disadvantage
+	};
+	
+	return __;
+})();
+
+var UI = {
+	highlight:function(ref){
+		var arr = document.getElementsByTagName("A");
+		for(var i=0; i<arr.length; i++){
+			var el = arr[i];
+			el.style.backgroundColor = el.name==ref?"yellow":"white";
+		}
+	},
+	toggleCriteria: function(problemId){
+		var a = document.getElementsByTagName("DIV");
+		for(var i=0; i<a.length; i++){
+			var el = a[i];
+			if(el.attributes.problemId&&el.attributes.problemId.value==problemId){
+				el.style.display = el.style.display!="none"?"none":"block";
+			}
+		}
+	},
+	toggle: function(id){
+		var div = document.getElementById("div"+id);
+		var hdr = document.getElementById("hdr"+id);
+		var collapse = div.style.display!="none";
+		div.style.display  = collapse?"none":"block";
+		hdr.title = collapse?"Развернуть":"Свернуть";
+	}
+};
