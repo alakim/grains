abfc83616ee8a8b47c733b465749755bf133fead
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2011-11-10 11:44:25 +0000

    Correlator: добавлен режим взаимной корреляция двух документов

--- correlator/correlator.js
+++ correlator/correlator.js
@@ -22,29 +22,44 @@ var Correlator = (function(){
 		return diff<0.6;
 	}
 	
+	function buildLines(txt){
+		var lines = txt.split("\n");
+		for(var i=0; i<lines.length; i++){
+			lines[i] = trim(lines[i]);
+		}
+		return lines;
+	}
+	
 	var _ = {
-		getMatrix: function(txt){
-			var lines = txt.split("\n");
-			for(var i=0; i<lines.length; i++){
-				lines[i] = trim(lines[i]);
-			}
+		getMatrix: function(txt, txt2){
+			var crossCorrelation = txt2!=null;
+			
+			var lines = buildLines(txt);
+			var lines2;
+			if(crossCorrelation) lines2 = buildLines(txt2);
+			
 			var dict = new FDict(lines);
 			var res = [];
-			each(lines, function(line, y){
+			(function(lines1, lines2){
+			each(lines2, function(line, y){
 				var sim = dict.find(line);
 				each(sim, function(sline){
 					var x = parseInt(sline.id);
-					if(x!=y && lines[x].length>minLineLength && lines[y].length>minLineLength && !differentLength(lines[x], lines[y])){
+					if((x!=y || crossCorrelation) 
+							&& (lines1[x].length>minLineLength && lines2[y].length>minLineLength )
+							&& !differentLength(lines1[x], lines2[y])
+						){
 						res.push({
 							x: x,
 							y: y,
 							v: sline.rate,
-							d1: lines[x],
-							d2: lines[y]
+							d1: lines1[x],
+							d2: lines2[y]
 						});
 					}
 				});
 			});
+			})(lines, crossCorrelation?lines2:lines);
 			return res;
 		}
 	};
