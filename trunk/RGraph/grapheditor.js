35428ec73fd4748f50103fe4325a9342540337b3
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2013-04-04 09:53:14 +0000

    Редактор графиков

---
+++ trunk/RGraph/grapheditor.js
@@ -0,0 +1,15 @@
+﻿var GraphEditor = (function($){
+	function build(panel){
+		panel.html(Html.div("Simple Graph Editor"));
+	}
+	
+	$.fn.graphEditor = function(){
+		$.each($(this), function(i, panel){
+			build($(panel));
+		});
+	}
+	
+	var __ = {
+	};
+	return __;
+})(jQuery);
