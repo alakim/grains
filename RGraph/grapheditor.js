6827d088a1339b8843c5f0fb3cee925cc04342a0
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2013-04-04 10:24:08 +0000

    Отображение таблицы редактора

--- RGraph/grapheditor.js
+++ RGraph/grapheditor.js
@@ -1,11 +1,32 @@
 ﻿var GraphEditor = (function($){
-	function build(panel){
-		panel.html(Html.div("Simple Graph Editor"));
+	var templates = {
+		main: function(data){with(Html){
+			return table({border:1, cellpadding:3, cellspacing:0},
+				tr(
+					td(),
+					apply(data.rowSettings, function(rs){
+						return th({style:style({color:rs.color})}, rs.name);
+					})
+				),
+				apply(data.labels, function(lbl, i){
+					return tr(
+						td(lbl), 
+						apply(data.rows, function(row){
+							return td(row[i]);
+						})
+					);
+				})
+			);
+		}}
+	};
+	
+	function build(panel, graphData){
+		panel.html(templates.main(graphData));
 	}
 	
-	$.fn.graphEditor = function(){
+	$.fn.graphEditor = function(graphData){
 		$.each($(this), function(i, panel){
-			build($(panel));
+			build($(panel), graphData);
 		});
 	}
 	
