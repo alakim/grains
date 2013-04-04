61dc93bd90dc19a4b3416c5336d467082f7b28e7
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2013-04-04 14:12:29 +0000

    Редактор графиков

--- trunk/RGraph/grapheditor.js
+++ trunk/RGraph/grapheditor.js
@@ -1,36 +1,47 @@
 ﻿var GraphEditor = (function($){
 	var templates = {
 		main: function(data){with(Html){
-			return table({border:1, cellpadding:3, cellspacing:0},
-				tr(
-					td(),
-					apply(data.rowSettings, function(rs){
-						return th({style:style({color:rs.color})}, rs.name);
-					})
-				),
-				apply(data.labels, function(lbl, i){
-					return tr(
-						td(lbl), 
-						apply(data.rows, function(row){
-							return td(row[i]);
+			return markup(
+				table({border:1, cellpadding:3, cellspacing:0},
+					tr(
+						td(),
+						apply(data.rowSettings, function(rs){
+							return th({style:style({color:rs.color})}, rs.name);
 						})
-					);
-				})
+					),
+					apply(data.labels, function(lbl, i){
+						return tr(
+							td(lbl), 
+							apply(data.rows, function(row, jRow){
+								return td(input({type:"text", value:row[i]||"", path:"rows/"+jRow+"/"+i}));
+							})
+						);
+					})
+				)
 			);
 		}}
 	};
 	
-	function build(panel, graphData){
-		panel.html(templates.main(graphData));
+	function build(panel){
+		panel.html(templates.main(__.data));
+		panel.find("input").change(function(){var _=$(this);
+			var val = _.val();
+			var path = _.attr("path");
+			JsPath.set(__.data, path, parseInt(val));
+			__.onchange(__.data);
+		});
 	}
 	
 	$.fn.graphEditor = function(graphData){
+		__.data = graphData;
 		$.each($(this), function(i, panel){
-			build($(panel), graphData);
+			build($(panel));
 		});
 	}
 	
 	var __ = {
+		data:null,
+		onchange: function(){}
 	};
 	return __;
 })(jQuery);
