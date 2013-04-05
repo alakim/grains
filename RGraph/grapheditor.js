1fb8dda85c31eec303cafc20d403f47e8a779950
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2013-04-05 08:10:28 +0000

    Добавление строки

--- RGraph/grapheditor.js
+++ RGraph/grapheditor.js
@@ -5,36 +5,57 @@
 				table({border:1, cellpadding:3, cellspacing:0},
 					tr(
 						td(),
-						apply(data.rowSettings, function(rs){
-							return th({style:style({color:rs.color})}, rs.name);
+						apply(data.rowSettings, function(rs, i){
+							return th({style:style({"background-color":rs.color})},
+								input({"class":"txtFld", type:"text", value:rs.name, path:"rowSettings/"+i+"/name"})
+							);
 						})
 					),
 					apply(data.labels, function(lbl, i){
-						return tr(
-							td(lbl), 
-							apply(data.rows, function(row, jRow){
-								return td(input({type:"text", value:row[i]||"", path:"rows/"+jRow+"/"+i}));
-							})
-						);
-					})
+						return templates.row(lbl, i, data);
+					}),
+					tr(td({colspan:data.rows.length+1}, input({type:"button", "class":"btnAddRow", value:"Добавить"})))
 				)
+			);
+		}},
+		row: function(lbl, i, data){with(Html){
+			return tr(
+				td(input({"class":"txtFld", type:"text", value:lbl, path:"labels/"+i})), 
+				apply(data.rows, function(row, jRow){
+					return td(input({"class":"dotFld", type:"text", value:row[i]||"", path:"rows/"+jRow+"/"+i}));
+				})
 			);
 		}}
 	};
 	
-	function build(panel){
-		panel.html(templates.main(__.data));
-		panel.find("input").change(function(){var _=$(this);
-			var val = _.val();
-			try{val = parseInt(val);}
-			catch(e){val = null;}
+	function bindEvents(panel){
+		panel.find("input.dotFld").change(function(){var _=$(this);
 			var path = _.attr("path");
+			var val = _.val();
+			try{val = parseFloat(val);}
+			catch(e){val = null;}
 			if(val)
 				JsPath.set(__.data, path, val);
 			else
 				JsPath.delItem(__.data, path);
 			__.onchange(__.data);
 		});
+		panel.find("input.txtFld").change(function(){var _=$(this);
+			var path = _.attr("path");
+			var val = _.val();
+			JsPath.set(__.data, path, val);
+			__.onchange(__.data);
+		});
+		panel.find(".btnAddRow").click(function(){
+			var row = panel.find("tr:last()");
+			var newRow = $(templates.row("", __.data.labels.length, __.data));
+			row.before(newRow);
+			bindEvents(newRow);
+		});
+	}
+	function build(panel){
+		panel.html(templates.main(__.data));
+		bindEvents(panel);
 	}
 	
 	$.fn.graphEditor = function(graphData){
