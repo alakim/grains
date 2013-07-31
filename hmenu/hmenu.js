8327ac8e3145e93a2b9218b742f9b60c99af5503
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2013-07-31 12:06:53 +0000

    Добавлен модуль hmenu.js

---
+++ hmenu/hmenu.js
@@ -0,0 +1,12 @@
+﻿(function($,H){
+	
+	function buildMenu(pnl){
+		pnl.show();
+	}
+	
+	$.fn.HMenu = function(){
+		$(this).each(function(i, el){
+			buildMenu($(el));
+		});
+	};
+})(jQuery, Html);
