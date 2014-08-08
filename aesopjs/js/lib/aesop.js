d7a03efe9ed982962d5d2a5c7df7f1bc9144ecb6
alvakim <alvakim@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2014-08-08 07:08:38 +0000

    Aesop v.2.3 - добавлен конструктор свойств, и соответствующее демо

--- aesopjs/js/lib/aesop.js
+++ aesopjs/js/lib/aesop.js
@@ -1,92 +1,134 @@
-﻿define([], function(){
-	var classIndex = {};
-	
-	function extend(obj, ext){
-		for(var k in ext) obj[k] = ext[k];
-	}
-	
-	function Class(name, func, facetConstructor){var _=this;
-		_.name = name;
-		_.func = func;
-		_.facets = {};
-		_.facetConstructor = facetConstructor || function(){};
-		classIndex[name] = _;
-	}
-	
-	extend(Class.prototype, {
-		each: function(act){var _=this;
-			var c = _.facets;
-			for(var id in c){
-				var el = c[id];
-				if(el) act(el);
-			}
-		},
-		map: function(F){
-			var res = [];
-			this.each(function(itm){res.push(F?F(itm):itm);});
-			return res;
-		},
-		getAll: function(){
-			return this.map();
+﻿(function(){
+	function Module(){
+		var classIndex = {};
+		
+		function extend(obj, ext){
+			for(var k in ext) obj[k] = ext[k];
 		}
-	});
-	
-	function addInstance(cls, itm, facetData){
-		if(cls.facets[itm.__aesopID]) return;
-		var fc = new cls.facetConstructor(itm);
-		fc.item = itm;
-		if(facetData) extend(fc, facetData);
-		cls.facets[itm.__aesopID] = fc;
-	}
-	
-	function removeInstance(cls, itm){
-		cls.facets[itm.__aesopID] = null;
-	}
-	
-	function getFacet(itm, classOrName){
-		if(!classOrName) alert("Class "+classOrName+" does not exist!");
-		var cls = typeof(classOrName)=="string"?classIndex[classOrName]:classOrName;
-		return cls.facets[itm.__aesopID];
-	}
-	
-	var newID = (function(){
-		var idx = 0;
-		return function(){
-			return ++idx;
-		};
-	})();
-	
-	return {
-		version: "2.2",
-		Class: Class,
-		classify: function(itm, className, facetData){
-			if(!itm.__aesopID) itm.__aesopID = newID();
-			if(!className){				for(var nm in classIndex){var cls = classIndex[nm];
-					if(!cls.func) continue;
-					if(cls.func(itm)) addInstance(cls, itm);
-					else removeInstance(cls, itm);
+		
+		function Class(name, func, facetConstructor){var _=this;
+			_.name = name;
+			_.func = func;
+			_.facets = {};
+			_.facetConstructor = facetConstructor || function(){};
+			classIndex[name] = _;
+		}
+		
+		extend(Class.prototype, {
+			each: function(act){var _=this;
+				var c = _.facets;
+				for(var id in c){
+					var el = c[id];
+					if(el) act(el);
 				}
+			},
+			map: function(F){
+				var res = [];
+				this.each(function(itm){res.push(F?F(itm):itm);});
+				return res;
+			},
+			getAll: function(){
+				return this.map();
 			}
-			else{
-				var cls = classIndex[className];
-				addInstance(cls, itm, facetData);
-			}
-		},
-		declassify: function(itm, className){
-			var cls = classIndex[className];
-			removeInstance(cls, itm);
-		},
-		getFacet: getFacet,
-		getClass: function(name){return classIndex[name];},
-		getClassNames: function(){
-			var res = [];
-			for(var nm in classIndex) res.push(nm);
-			return res;
-		},
-		getInstancesCount: function(classNm){
-			var res = 0, c = classIndex[classNm].facets;
-			for(var k in c) if(c[k]) res++;
-			return res;
+		});
+		
+		function addInstance(cls, itm, facetData){
+			if(cls.facets[itm.__aesopID]) return;
+			var fc = new cls.facetConstructor(itm);
+			fc.item = itm;
+			if(facetData) extend(fc, facetData);
+			cls.facets[itm.__aesopID] = fc;
 		}
-	};
-});
+		
+		function removeInstance(cls, itm){
+			cls.facets[itm.__aesopID] = null;
+		}
+		
+		function getFacet(itm, classOrName){
+			if(!classOrName) alert("Class "+classOrName+" does not exist!");
+			var cls = typeof(classOrName)=="string"?classIndex[classOrName]:classOrName;
+			return cls.facets[itm.__aesopID];
+		}
+		
+		var newID = (function(){
+			var idx = 0;
+			return function(){
+				return ++idx;
+			};
+		})();
+		
+		function property(owner, initVal, onchange){
+			var owner = owner,
+				val = initVal,
+				eventHandlers = [],
+				readonly = false;
+			if(onchange) eventHandlers.push(onchange);
+				
+			function F(v){
+				if(v && !readonly){
+					val = v;
+					for(var e,c=eventHandlers,i=0; e=c[i],i<c.length; i++){
+						if(e)e(owner, v);
+					}
+				}
+				else return val;
+			}
+			F.readonly = function(){
+				readonly = true;
+				return this;
+			}
+			F.bind = function(handler){
+				eventHandlers.push(handler);
+				return eventHandlers.length - 1;
+			};
+			
+			F.unbind = function(idx){
+				var res = [];
+				for(var h,c=eventHandlers,i=0; h=c[i],i<c.length; i++){
+					if(i!=idx) res.push(h);
+				}
+				eventHandlers = res;
+			}
+			return F;
+		}
+
+		
+		return {
+			version: "2.3",
+			Class: Class,
+			classify: function(itm, className, facetData){
+				if(!itm.__aesopID) itm.__aesopID = newID();
+				if(!className){					for(var nm in classIndex){var cls = classIndex[nm];
+						if(!cls.func) continue;
+						if(cls.func(itm)) addInstance(cls, itm);
+						else removeInstance(cls, itm);
+					}
+				}
+				else{
+					var cls = classIndex[className];
+					addInstance(cls, itm, facetData);
+				}
+			},
+			declassify: function(itm, className){
+				var cls = classIndex[className];
+				removeInstance(cls, itm);
+			},
+			getFacet: getFacet,
+			getClass: function(name){return classIndex[name];},
+			getClassNames: function(){
+				var res = [];
+				for(var nm in classIndex) res.push(nm);
+				return res;
+			},
+			getInstancesCount: function(classNm){
+				var res = 0, c = classIndex[classNm].facets;
+				for(var k in c) if(c[k]) res++;
+				return res;
+			},
+			property: property
+		};
+	}
+	
+	if(typeof(define)=="function") define([], Module);
+	else Aesop = Module();
+})();
