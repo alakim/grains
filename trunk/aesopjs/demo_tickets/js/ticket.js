f08e0074b6b37c8f8c1e69cfd608302f7d99f877
alvakim@gmail.com <alvakim@gmail.com@5b4bf79c-cedf-11dd-94fd-9de6169c3690>
2015-03-06 12:19:09 +0000

    New Aesop Demo - формирование заявок на обслуживание

---
+++ trunk/aesopjs/demo_tickets/js/ticket.js
@@ -0,0 +1,24 @@
+﻿define(["jquery", "html", "aesop"], function($, $H, $A){
+	var instCounter = 1;
+	
+	new $A.Class("Ticket",
+		function(inst){
+			return inst.ticketID!=null;
+		},
+		function(){
+			this.ticketID = instCounter++;
+			this.view = function(){return templates.mainList(this)};
+		}
+	);
+
+	var templates = {
+		mainList: function(itm){with($H){
+			return markup(
+				"Ticket #",
+				itm.ticketID
+			);
+		}}
+	};
+
+	return {};
+});
