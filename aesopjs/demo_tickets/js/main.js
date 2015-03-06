fcffb127149da95210e1b6b3fca7acc6451fbb82
alvakim <alvakim@gmail.com>
2015-03-06 12:19:09 +0000

    New Aesop Demo - формирование заявок на обслуживание

---
+++ aesopjs/demo_tickets/js/main.js
@@ -0,0 +1,37 @@
+﻿requirejs.config({
+    baseUrl: "js",
+    paths: {
+		jquery: "../../js/lib/jquery-1.11.0.min",
+		html:"../../js/lib/html",
+		aesop:"../../js/lib/aesop"
+    },
+	urlArgs: "bust=" + (new Date()).getTime(),
+	shim:{
+		"html":{exports:"Html"}
+	}
+});
+
+requirejs(["jquery", "html", "aesop", "ticket"], function($, $H, $A, Ticket) {
+	$("#version").html($H.div("Powered by AesopJS v."+$A.version));
+	
+	$A.classify({}, "Ticket");
+	$A.classify({}, "Ticket");
+	$A.classify({}, "Ticket");
+	
+	function classList(list){with($H){
+		return div(
+			ol(
+				apply(list, function(itm){
+					return li(itm.view());
+				})
+			)
+		);
+	}}
+	
+	$(function(){
+		$("#mainView").html(classList(
+			$A.getClass("Ticket").getAll()
+		));
+	});
+	
+});
