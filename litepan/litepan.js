4d973020bf8a21f84c98793404da6631829731d2
Alexander Akimow <AkimovAV6@str.mos.ru>
2017-04-11 10:24:41 +0300

    LitePan movements

--- litepan/litepan.js
+++ litepan/litepan.js
@@ -1,4 +1,5 @@
 var LitePan = (function($, $H, $S){
+	var fixY = false;
 	
 	function extend(o, S){
 		for(var k in S) o[k] = S[k];
@@ -6,54 +7,68 @@ var LitePan = (function($, $H, $S){
 
 	var panGroup = null;
 	var size = {w:100, h:100};
+	var paper = null;
 
-	function LitePan(pnl, imgRef, w, h){pnl=$(pnl);
-		var snp = $($H.svg({width:w, height:h, version:'1.1', xmlns:'http://w3.org/2000/svg'}));
-		size = {w:w, h:h};
+	var px = $H.unit('px');
+
+	function LitePan(pnl, imgRef, imgSize, frmSize){pnl=$(pnl);
+		pnl.css({width:px(frmSize.w), height: px(frmSize.h), overflow:'hidden', cursor:'move'});
+		var snp = $($H.svg({width:frmSize.w, height:frmSize.h, version:'1.1', xmlns:'http://w3.org/2000/svg'}));
+		size = imgSize;
 		pnl.append(snp);
-		var paper = Snap(snp[0]);
-		paper.rect(0, 0, w, h).attr({
+		paper = Snap(snp[0]);
+		paper.rect(0, 0, imgSize.w, imgSize.h).attr({
 			fill: '#fff',
 			stroke: '#f00'
 		});
 
 
 		var catched = false;
+		var t0 = {x:0, y:0};
 
 		panGroup = paper.group(
-			paper.image(imgRef, -w, 0, w, h),
-			paper.image(imgRef, 0, 0, w, h),
-			paper.image(imgRef, w, 0, w, h)
+			paper.image(imgRef, -imgSize.w, 0, imgSize.w, imgSize.h),
+			paper.image(imgRef, 0, 0, imgSize.w, imgSize.h),
+			paper.image(imgRef, imgSize.w, 0, imgSize.w, imgSize.h)
 		)
-		.mousedown(function(ev){
-			// console.log('down: %o', ev.movementX);
-			catched = true;
-			ev.stopPropagation();
-		})
-		.mouseup(function(ev){
-			// console.log('up: %o', ev.movementX);
-			catched = false;
-			ev.stopPropagation();
-		})
-		.mousemove(function(ev){
-			if(!catched) return;
-			var dX = ev.movementX;
-			// console.log('move: %o', dX);
-			move(dX);
-		});
+		.drag(
+			function(dx, dy){
+				if(catched) move(dx, dy, t0);
+			},
+			function(){
+				catched = true;
+				var mtx = panGroup.attr('transform').totalMatrix;
+				t0 = {x: mtx.e, y: mtx.f};
+			},
+			function(){
+				catched = false;
+				t0 = {x:0, y:0};
+			}
+		);
 	}
 	
-	function move(c){
+	function move(dx, dy, t0){
 		if(!panGroup) return;
-		var t0 = panGroup.attr('transform').totalMatrix;
-		var t1 = t0.e + c;
-		if(t1<-size.w) t1 = t1+size.w;
-		else if(t1>size.w) t1 = t1-size.w;
-		panGroup.transform('T'+t1+',0');
+		t0 = t0 || {x:0, y:0};
+		var x = t0.x + dx,
+			y = t0.y + dy;
+		if(x<-size.w) x = x+size.w;
+		else if(x>size.w) x = x-size.w;
+
+		if(y>0) y = 0;
+		else if(y<(frmSize.h - imgSize.h)) y = frmSize.h - imgSize.h;
+		
+		if(fixY) panGroup.transform('T'+x+',0');
+		else panGroup.transform('T'+x+','+y);
+	}
+
+	function getPaper(){
+		return paper;
 	}
 
 	extend(LitePan, {
-		move: move
+		move: move,
+		getPaper: getPaper
 	});
 
 	return LitePan;
