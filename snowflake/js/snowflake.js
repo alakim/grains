6878a8ca7873183a31a71107ba0afa63cc11d96f
alakim <alakm@rambler.ru>
2019-11-06 17:59:04 +0300

    Snowflake branch path improved

--- snowflake/js/snowflake.js
+++ snowflake/js/snowflake.js
@@ -3,18 +3,28 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 	const strokeWidth = 2;
 
 	function branch(paper, root, directions, size){
-		const path = ['M', root.x, root.y, 'L', root.x, root.y+size].join(' ');
+		//let path = ['M', root.x, root.y, 'L', root.x, root.y+size];
+		let path = [];
 		for(dir of directions){
-			paper.path(path)
-				.attr({stroke:color, strokeWidth:strokeWidth})
-				.transform(['rotate(', dir, root.x, root.y, ')'].join(' '))
-			;
+			const dR = dir*Math.PI/180;
+			// paper.circle(root.x, root.y, 5).attr({fill:'#ffff00'});
+			const pt = [root.x+Math.cos(dR)*size, root.y+Math.sin(dR)*size];
+			// paper.circle(...pt, 5).attr({fill:'#00ff00'});
+			path = path.concat(['M', root.x, root.y, 'L', ...pt]); //root.x+dir, root.y+dir]);
 		}
+
+		paper.path(path.join(' ')).attr({stroke:color, strokeWidth:strokeWidth});
+		// const nodePos = .6;
+		// const node = {
+		// 	x: root.x,
+		// 	y: root.y+size*nodePos
+		// };
+		// paper.circle(node.x, node.y, 5).attr({fill:'#ff0000'});
 	}
 
 	function draw(){
 		const paper = $S('#main');
-		const shape = {x:30, y:30, size:20};
+		const shape = {x:130, y:130, size:80};
 		branch(paper, {x,y}=shape, Array.from(hexRay()), shape.size);
 	}
 
