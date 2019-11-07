97ea55132c3cd7d1b62c28d42ecea5195aac2ab1
alakim <alakm@rambler.ru>
2019-11-07 13:16:13 +0300

    Simple snowflake path drawing

--- snowflake/js/snowflake.js
+++ snowflake/js/snowflake.js
@@ -1,31 +1,41 @@
 const Snowflake = (function($, $C, $S){const $H = $C.simple;
 	const color = '#eeeeff';
 	const strokeWidth = 2;
+	let paper;
 
-	function branch(paper, root, directions, size){
-		//let path = ['M', root.x, root.y, 'L', root.x, root.y+size];
+	let branchLevel = 2;
+
+	function branch(root, directions, size, level=0){
+		console.log(root, directions, size);
+		if(level>branchLevel) return;
+		if(size<1) return;
+
 		let path = [];
 		for(dir of directions){
 			const dR = dir*Math.PI/180;
-			// paper.circle(root.x, root.y, 5).attr({fill:'#ffff00'});
-			const pt = [root.x+Math.cos(dR)*size, root.y+Math.sin(dR)*size];
-			// paper.circle(...pt, 5).attr({fill:'#00ff00'});
-			path = path.concat(['M', root.x, root.y, 'L', ...pt]); //root.x+dir, root.y+dir]);
+			[x, y] = root;
+
+			function ptAtLng(lng){
+				return [x+Math.cos(dR)*lng, y+Math.sin(dR)*lng];
+			}
+
+			const pt = ptAtLng(size);
+			path = path.concat(['M', x, y, 'L', ...pt]);
+
+			const node = ptAtLng(size*.7);
+			// paper.circle(...node, 5).attr({fill:'#00ff00'});
+			
+			if(branchLevel>0) path = path.concat(branch(node, [dir+60, dir-60], size*.4, level+1));
 		}
 
-		paper.path(path.join(' ')).attr({stroke:color, strokeWidth:strokeWidth});
-		// const nodePos = .6;
-		// const node = {
-		// 	x: root.x,
-		// 	y: root.y+size*nodePos
-		// };
-		// paper.circle(node.x, node.y, 5).attr({fill:'#ff0000'});
+		return path;
 	}
 
 	function draw(){
-		const paper = $S('#main');
+		paper = $S('#main');
 		const shape = {x:130, y:130, size:80};
-		branch(paper, {x,y}=shape, Array.from(hexRay()), shape.size);
+		const path = branch([shape.x, shape.y], Array.from(hexRay()), shape.size);
+		paper.path(path.join(' ')).attr({stroke:color, strokeWidth:strokeWidth});
 	}
 
 	function* hexRay(){
