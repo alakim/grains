cd60c5093208fa4154eca2e0964314db848a2290
alakim <alakm@rambler.ru>
2019-11-11 10:20:54 +0300

    Snowflake new buildPath method

--- snowflake/js/snowflake.js
+++ snowflake/js/snowflake.js
@@ -12,58 +12,63 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 		return .8 * (rr+.1)*lng;
 	}
 
-	const levelSizes = [];
-	const levelNodes = [];
-
-	function branch(root, directions, level=0){
-		let size = levelSizes[level];
-		if(!size){
-			size = randomLength(levelSizes[level-1]);
-			levelSizes[level] = size;
-		}
-
-		let nodeLng = levelNodes[level];
-		if(!nodeLng){
-			nodeLng = randomLength(size);
-			levelNodes[level] = nodeLng;
-		}
-
-		// console.log(root, directions, size);
-		if(level>branchLevel) return;
-		if(size<1) return;
-
-		let path = [];
-		for(dir of directions){
-			const dR = dir*Math.PI/180;
-			[x, y] = root;
-
-			function ptAtLng(lng){
-				return [x+Math.cos(dR)*lng, y+Math.sin(dR)*lng];
-			}
-
-			const pt = ptAtLng(size);
-			path = path.concat(['M', x, y, 'L', ...pt]);
-
-			const node = ptAtLng(nodeLng);
-			// paper.circle(...node, 5).attr({fill:'#00ff00'});
-			
-			if(branchLevel>0) path = path.concat(branch(node, [dir+60, dir-60], level+1));
-		}
-
-		return path;
-	}
-
 	function draw(snapPaper, pos, size){
 		paper = snapPaper;
-		levelSizes[0] = size;
-		levelNodes[0] = 0;
-		const path = branch(pos, Array.from(hexRay()));
-		paper.path(path.join(' ')).attr({stroke:color, strokeWidth:strokeWidth});
+		const path = buildPath(pos, size);
+		paper.path(path).attr({stroke:color, strokeWidth:strokeWidth});
 	}
 
 	function* hexRay(){
 		for(let i=0; i<6; i++)
 			yield 60*i;
+	}
+
+	function buildPath(pos, size){
+
+		const levelSizes = [];
+		const levelNodes = [];
+
+		function branch(root, directions, level=0){
+			let size = levelSizes[level];
+			if(!size){
+				size = randomLength(levelSizes[level-1]);
+				levelSizes[level] = size;
+			}
+
+			let nodeLng = levelNodes[level];
+			if(!nodeLng){
+				nodeLng = randomLength(size);
+				levelNodes[level] = nodeLng;
+			}
+
+			// console.log(root, directions, size);
+			if(level>branchLevel) return;
+			if(size<1) return;
+
+			let path = [];
+			for(dir of directions){
+				const dR = dir*Math.PI/180;
+				[x, y] = root;
+
+				function ptAtLng(lng){
+					return [x+Math.cos(dR)*lng, y+Math.sin(dR)*lng];
+				}
+
+				const pt = ptAtLng(size);
+				path = path.concat(['M', x, y, 'L', ...pt]);
+
+				const node = ptAtLng(nodeLng);
+				// paper.circle(...node, 5).attr({fill:'#00ff00'});
+				
+				if(branchLevel>0) path = path.concat(branch(node, [dir+60, dir-60], level+1));
+			}
+
+			return path;
+		}
+
+		levelSizes[0] = size;
+		levelNodes[0] = 0;
+		return branch(pos, Array.from(hexRay())).join(' ');
 	}
 
 	return {draw};
