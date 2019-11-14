4375ab311fe8c93aa99c763e7ebc74bc5400175f
alakim <alakm@rambler.ru>
2019-11-14 11:47:54 +0300

    Snowflake double branch mode added

--- snowflake/js/snowflake.js
+++ snowflake/js/snowflake.js
@@ -3,13 +3,18 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 	const strokeWidth = 2;
 	let paper;
 
-	let branchLevel = 2;
+	const Settings = {
+		branchLevel: 2,
+		doubleBranchesPropability: .7,
+		highlightNodes: false,
+		levelLengthFactor: .7
+	};
 
 	function randomLength(lng){
 		//return lng*.4;
 		// const ss = new Date().getTime();
 		const rr = Math.random();
-		return .8 * (rr+.1)*lng;
+		return Settings.levelLengthFactor * (rr+.1)*lng;
 	}
 
 	function draw(snapPaper, pos, size){
@@ -28,6 +33,10 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 		const levelSizes = [];
 		const levelNodes = [];
 
+		function branchCount(){
+			return Math.random()>Settings.doubleBranchesPropability?1:2;
+		}
+
 		function branch(root, directions, level=0){
 			let size = levelSizes[level];
 			if(!size){
@@ -37,30 +46,48 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 
 			let nodeLng = levelNodes[level];
 			if(!nodeLng){
-				nodeLng = randomLength(size);
+				const bCount = branchCount();
+				if(level===0 && bCount>1){
+					nodeLng = [];
+					for(let i=0; i<bCount; i++){
+						nodeLng[i] = i*size/2 + randomLength(size/2);
+					}
+				}
+				else{
+					nodeLng = randomLength(size);
+
+				}
 				levelNodes[level] = nodeLng;
 			}
 
 			// console.log(root, directions, size);
-			if(level>branchLevel) return;
+			if(level>Settings.branchLevel) return;
 			if(size<1) return;
 
 			let path = [];
-			for(dir of directions){
+			for(let dir of directions){
 				const dR = dir*Math.PI/180;
-				[x, y] = root;
 
 				function ptAtLng(lng){
-					return [x+Math.cos(dR)*lng, y+Math.sin(dR)*lng];
+					return [root[0]+Math.cos(dR)*lng, root[1]+Math.sin(dR)*lng];
 				}
 
 				const pt = ptAtLng(size);
-				path = path.concat(['M', x, y, 'L', ...pt]);
+				path = path.concat(['M', root[0], root[1], 'L', ...pt]);
 
-				const node = ptAtLng(nodeLng);
-				// paper.circle(...node, 5).attr({fill:'#00ff00'});
-				
-				if(branchLevel>0) path = path.concat(branch(node, [dir+60, dir-60], level+1));
+				function addNode(pos, xIdx){
+					const node = ptAtLng(pos);
+					// if(xIdx!=null) console.log(xIdx, pos);
+					//console.log('xIdx: %o', xIdx);
+					if(Settings.highlightNodes && xIdx!=null) paper.circle(...node, 5).attr({fill:(xIdx?'#ff0000':'#00ff00')});
+					// console.log('root: %o, x: %o, y:%o, xIdx: %o', root, x, y, xIdx);
+					if(Settings.branchLevel>0) path = path.concat(branch(node, [dir+60, dir-60], level+1));
+				}
+				if(nodeLng instanceof Array){
+					for(let i=0; i<nodeLng.length; i++) addNode(nodeLng[i], i);
+					// const i = 1; addNode(nodeLng[i], i);
+				}
+				else addNode(nodeLng);
 			}
 
 			return path;
@@ -68,9 +95,13 @@ const Snowflake = (function($, $C, $S){const $H = $C.simple;
 
 		levelSizes[0] = size;
 		levelNodes[0] = 0;
-		return branch(pos, Array.from(hexRay())).join(' ');
+		const snowflake = branch(pos, Array.from(hexRay())).join(' ');
+		// console.log('levelNodes: %o', levelNodes);
+		return snowflake;
 	}
 
-	return {draw, buildPath};
+	return {draw, buildPath,
+		settings: function(S){$C.extend(Settings, S);}
+	};
 
 })(jQuery, Clarino.version('1.4.0'), Snap);
